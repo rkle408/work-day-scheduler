ae86d431b99ef935a0d61bed1709f1ec0c095aa5
Rosebud <rkle408@gmail.com>
2022-09-08 20:01:24 -0700

    add save button that stores data to local storage

--- script.js
+++ script.js
@@ -16,23 +16,34 @@ var saveButtonEl = $(".saveBtn");
 // TO DO:
 // Append things to containerEl
 containerEl.append(descriptionEl);
+containerEl.append(saveButtonEl);
+
+// Need to make timeblocks
 
 // Will need a save button event with preventDefault:
-// saveButtonEl.on('click', function(event) {
-//  event.preventDefault();
-//  
-// 
-//  // Need localStorage
-// localStorage.setItem("descriptionEl", JSON.stringify(descriptionEl));
-// });
+saveButtonEl.on('click', function(event) {
+    event.preventDefault();
+  
+    // Need localStorage
+    localStorage.setItem("descriptionEl", JSON.stringify(descriptionEl));
+    renderDescription(); 
+});
+
+// need to display saved description and get localStorage
+function renderDescription () {
+    var plans = JSON.parse(localStorage.getItem("descriptionEl"));
+    if (plans !== null) {
+        textareaEl.text(descriptionEl.value);
+    }
+}
+
+// Need loop for every hour?
 
 // Function to display time
 function displayTime () {
     var timeNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     currentDayElement.text(timeNow);
 }
-
-
 
 // Declare function
 setInterval(displayTime, 1000);
