968c58f5cb2d85f6cf53d63d2e91ecd6543338b2
Rosebud <rkle408@gmail.com>
2022-09-09 13:54:49 -0700

    updated unique variables for text and button

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function renderDescription() {
 // Need loop for every hour?
 
 
-// Function to display time
+// Function to display time in jumbotron
 function displayTime() {
     var timeNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     currentDayElement.text(timeNow);
@@ -48,6 +48,31 @@ setInterval(displayTime, 1000);
 
 
 // Need variables for every hour
+var sevenAmText = $('#7amtext');
+var eightAmText = $('#8amtext');
+var nineAmText = $('#9amtext');
+var tenAmText = $('#10amtext');
+var elevenAmText = $('#11amtext');
 
+var twelvePmText = $('#12pmtext');
+var onePmText = $('#1pmtext');
+var twoPmText = $('#2pmtext');
+var threePmText = $('#3pmtext');
+var fourPmText = $('#4pmtext');
+var fivePmText = $('#5pmtext');
+
+// Need variables for every button
+var sevenAmBtn = $('.7amsaveBtn');
+var eightAmBtn = $('.8amsaveBtn');
+var nineAmBtn = $('.9amsaveBtn');
+var tenAmBtn = $('.10amsaveBtn');
+var elevenAmBtn = $('.11amsaveBtn');
+
+var twelvePmBtn = $('.12pmsaveBtn');
+var onePmBtn = $('.1pmsaveBtn');
+var twoPmBtn = $('.2pmsaveBtn');
+var threePmBtn = $('.3pmsaveBtn');
+var fourPmBtn = $('.4pmsaveBtn');
+var fivePmBtn = $('.5pmsaveBtn');
 
 // Need to do functions for every hour
