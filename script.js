f88d5362a3a920772301320223093a86a6336ea3
Rosebud <rkle408@gmail.com>
2022-09-09 15:13:46 -0700

    add 9 to 12

--- script.js
+++ script.js
@@ -46,14 +46,14 @@ sevenAmBtn.on('click', function(event) {
     // Need localStorage
     console.log($("#7amText").val());
     localStorage.setItem("sevenAm", $("#7amText").val());
-    console.log("saved");
+    // console.log("saved");
 });
 
-let sevenAmStored = localStorage.getItem("sevenAm");
+var sevenAmStored = localStorage.getItem("sevenAm");
 sevenAmText.val(sevenAmStored);
 
 // Need to get past, present, future colors
-let now = moment().hour();
+var now = moment().hour();
 
 if (now > 7) {
     $('#7amText').addClass('past');
@@ -68,16 +68,13 @@ if (now > 7) {
 eightAmBtn.on('click', function(event) {
     event.preventDefault();
 
-    // Need localStorage
     console.log($("#8amText").val());
     localStorage.setItem("eightAm", $("#8amText").val());
-    console.log("saved");
+    // console.log("saved");
 });
 
-let eightAmStored = localStorage.getItem("eightAm");
-sevenAmText.val(sevenAmStored);
-
-// Need to get past, present, future colors
+var eightAmStored = localStorage.getItem("eightAm");
+eightAmText.val(eightAmStored);
 
 if (now > 8) {
     $('#8amText').addClass('past');
@@ -85,4 +82,88 @@ if (now > 8) {
     $('#8amText').addClass('present');
 } else { 
     $('#8amText').addClass('future');
+}
+
+// 
+
+nineAmBtn.on('click', function(event) {
+    event.preventDefault();
+
+    console.log($("#9amText").val());
+    localStorage.setItem("nineAm", $("#9amText").val());
+    // console.log("saved");
+});
+
+var nineAmStored = localStorage.getItem("nineAm");
+nineAmText.val(nineAmStored);
+
+if (now > 9) {
+    $('#9amText').addClass('past');
+} else if (now === 9) {
+    $('#9amText').addClass('present');
+} else { 
+    $('#9amText').addClass('future');
+}
+
+//
+
+tenAmBtn.on('click', function(event) {
+    event.preventDefault();
+
+    console.log($("#10amText").val());
+    localStorage.setItem("tenAm", $("#10amText").val());
+    //console.log("saved");
+});
+
+var tenAmStored = localStorage.getItem("tenAm");
+tenAmText.val(tenAmStored);
+
+if (now > 10) {
+    $('#10amText').addClass('past');
+} else if (now === 10) {
+    $('#10amText').addClass('present');
+} else { 
+    $('#10amText').addClass('future');
+}
+
+//
+
+elevenAmBtn.on('click', function(event) {
+    event.preventDefault();
+
+    console.log($("#11amText").val());
+    localStorage.setItem("elevenAm", $("#11amText").val());
+    // console.log("saved");
+});
+
+var elevenAmStored = localStorage.getItem("elevenAm");
+elevenAmText.val(elevenAmStored);
+
+if (now > 11) {
+    $('#11amText').addClass('past');
+} else if (now === 11) {
+    $('#11amText').addClass('present');
+} else { 
+    $('#11amText').addClass('future');
+}
+
+//
+
+twelvePmBtn.on('click', function(event) {
+    event.preventDefault();
+
+    console.log($("#12pmText").val());
+    localStorage.setItem("twelvePm", $("#12pmText").val());
+    // console.log("saved");
+});
+
+var twelvePmStored = localStorage.getItem("twelvePm");
+twelvePmText.val(twelvePmStored);
+
+if (now > 12) {
+    $('#12pmText').addClass('past');
+} else if (now === 12) {
+    $('#12pmText').addClass('present');
+} else { 
+    $('#12pmText').addClass('future');
 }
