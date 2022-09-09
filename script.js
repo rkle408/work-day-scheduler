f9fdfda6d65e551abc96add4b747177559735165
Rosebud <rkle408@gmail.com>
2022-09-08 19:29:14 -0700

    add text box, more notes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Confirmed Variables Here:
+var currentDayElement = $("#currentDay");
+var containerEl = $(".container");
+var textareaEl = $("textarea");
+
 // Need to use these elements, but they may be moved into functions later!:
 var descriptionEl = $(".description");
 var timeBlockEl = $(".time-block");
@@ -8,14 +13,18 @@ var presentEl = $(".present");
 var futureEL = $(".future");
 var saveButtonEl = $(".saveBtn");
 
-// Need localStorage
-// Will need a save button event
-// 
+// TO DO:
+// Append things to containerEl
+containerEl.append(descriptionEl);
 
-// Confirmed Variables Here:
-var currentDayElement = $("#currentDay");
-var containerEl = $(".container");
-var textareaEl = $("textarea");
+// Will need a save button event with preventDefault:
+// saveButtonEl.on('click', function(event) {
+//  event.preventDefault();
+//  
+// 
+//  // Need localStorage
+// localStorage.setItem("descriptionEl", JSON.stringify(descriptionEl));
+// });
 
 // Function to display time
 function displayTime () {
